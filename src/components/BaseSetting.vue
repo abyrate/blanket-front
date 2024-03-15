b96ab26b694e9074e53d883ddfaa6538dd51abd4
asboldyrev <angelcorpc@ya.ru>
2024-03-15 13:17:41 +0700

    Добавил чекбокс для показа полей с сидами

--- src/components/BaseSetting.vue
+++ src/components/BaseSetting.vue
@@ -12,10 +12,18 @@
         <input id="colors" v-model="mainStore.numColors" min="0" class="form-control" type="number">
     </div>
     <div class="mb-3">
+        <div class="form-check">
+            <input id="flexCheckDefault" v-model="showSeeds" class="form-check-input" type="checkbox">
+            <label class="form-check-label" for="flexCheckDefault">
+                Настроить варианты
+            </label>
+        </div>
+    </div>
+    <div v-if="showSeeds" class="mb-3">
         <label for="seedPalette" class="form-label">Вариант палитры:</label>
         <input id="seedPalette" v-model="mainStore.seedPalette" min="0" class="form-control" type="number">
     </div>
-    <div class="mb-3">
+    <div v-if="showSeeds" class="mb-3">
         <label for="seedCombination" class="form-label">Вариант комбинации:</label>
         <div class="input-group mb-3">
             <div class="input-group-text">
@@ -28,8 +36,11 @@
 
 <script setup>
 import { useMainStore } from '@/store'
+import { ref } from 'vue'
 
 const mainStore = useMainStore()
+
+const showSeeds = ref(false)
 </script>
 
 <style lang="scss" scoped>
