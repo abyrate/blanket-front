39b2f6ef9bf0060b83e9be7300a5ec994f631179
asboldyrev <angelcorpc@ya.ru>
2024-03-15 20:41:23 +0700

    Поменял стартовые значения полей

--- src/store.js
+++ src/store.js
@@ -2,9 +2,9 @@ import { defineStore } from 'pinia'
 import { ref } from 'vue'
 
 export const useMainStore = defineStore('main', () => {
-    const width = ref(1)
-    const height = ref(1)
-    const numColors = ref(1)
+    const width = ref(2)
+    const height = ref(3)
+    const numColors = ref(3)
     const seedPalette = ref(Math.floor(Math.random() * 10000))
     const fixedCombinationSeed = ref(false)
     const combinationSeed = ref(Math.floor(Math.random() * 10000))
